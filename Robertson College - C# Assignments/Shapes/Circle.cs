b5185307c95a323e01a7a854a911b4495fd363c3
Ian Abad <51462584+ianabad@users.noreply.github.com>
2022-05-14 00:03:46 -0500

    Add shapes app w/o the packages

---
+++ Robertson College - C# Assignments/Shapes/Circle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Shapes
+{
+    [Serializable()]
+    public class Circle : Shape
+    {
+        public Circle() { }
+
+        public double Radius { get; set; }
+
+        public override string Name
+        {
+            get { return "Circle"; }
+        }
+
+        public override double Area
+        {
+            get { return Radius * Radius * Math.PI; }
+        }
+    }
+}
